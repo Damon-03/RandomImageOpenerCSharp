8e20fd05082e719b4ebc8a0eb0940097b37e80e9
Damon-03 <73558661+Damon-03@users.noreply.github.com>
2021-01-06 07:55:59 +1300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -16,22 +16,67 @@ namespace ImageOpenerCsharp
         {
             InitializeComponent();
         }
-
+        public void getImage(string location)
+        {
+            var rand = new Random();
+            var files = System.IO.Directory.GetFiles(location, "*", System.IO.SearchOption.AllDirectories);
+            var imageChoice = files[rand.Next(files.Length)];
+            pictureBox1.ImageLocation = imageChoice;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Size = new Size(1280, 720);
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
-        
-            
-            
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            var rand = new Random();
-            var files = System.IO.Directory.GetFiles("D://Refrences/Folder 1","*.jpg");
-            var imageChoice = files[rand.Next(files.Length)];
-            pictureBox1.ImageLocation = imageChoice;
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.Size = new Size(1280, 720);   
+  
+        }
+        /*
+        private void button1(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 1");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 2");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 3");
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 4");
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 5");
+        }
+        */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 1");
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            getImage("D:/Refrences/Folder 5");
         }
     }
 }
